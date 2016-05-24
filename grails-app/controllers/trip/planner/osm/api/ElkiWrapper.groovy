c37c8810ec38930088d2680d9db9cfe5bbf55d95
Peter Nancke <peter.nancke@gmx.de>
2016-05-24 19:30:25 +0200

    Find outliers with clustering; change max time to additional time

---
+++ grails-app/controllers/trip/planner/osm/api/ElkiWrapper.groovy
@@ -0,0 +1,67 @@
+package trip.planner.osm.api
+
+import de.lmu.ifi.dbs.elki.algorithm.clustering.kmeans.KMeansLloyd
+import de.lmu.ifi.dbs.elki.algorithm.clustering.kmeans.initialization.RandomlyGeneratedInitialMeans
+import de.lmu.ifi.dbs.elki.data.Cluster
+import de.lmu.ifi.dbs.elki.data.Clustering
+import de.lmu.ifi.dbs.elki.data.NumberVector
+import de.lmu.ifi.dbs.elki.data.model.KMeansModel
+import de.lmu.ifi.dbs.elki.database.Database
+import de.lmu.ifi.dbs.elki.database.StaticArrayDatabase
+import de.lmu.ifi.dbs.elki.datasource.ArrayAdapterDatabaseConnection
+import de.lmu.ifi.dbs.elki.datasource.DatabaseConnection
+import de.lmu.ifi.dbs.elki.distance.distancefunction.geo.LatLngDistanceFunction
+import de.lmu.ifi.dbs.elki.logging.LoggingConfiguration
+import de.lmu.ifi.dbs.elki.math.geodesy.SphericalVincentyEarthModel
+import de.lmu.ifi.dbs.elki.math.random.RandomFactory
+import trip.planner.osm.model.Node
+
+class ElkiWrapper {
+
+    static ArrayList<Cluster> getClusters(ArrayList<Node> nodes, int kMeansPartitions, int kMeansIterations) {
+        LoggingConfiguration.setStatistics();
+        double[][] data = convertNodeList(nodes)
+        StaticArrayDatabase db = getDatabase(data)
+        Clustering<KMeansModel> c = getKMeans(kMeansPartitions, kMeansIterations).run(db);
+        return c.getAllClusters()
+    }
+
+    public static ArrayList<Cluster> filterOutliers(ArrayList<Cluster> allClusters,
+                                                    int kMeansPartitions,
+                                                    double minMeanPercentageClusterSize) {
+
+        int allPointsCount = 0
+        allClusters.each { allPointsCount += it.size() }
+        int averageSize = allPointsCount / kMeansPartitions
+        ArrayList<Cluster> filteredClusters = new ArrayList<>()
+        allClusters.each {
+            if (it.size() > minMeanPercentageClusterSize * averageSize) {
+                filteredClusters.add(it)
+            }
+        }
+        filteredClusters
+    }
+
+    private static KMeansLloyd<NumberVector> getKMeans(int kMeansPartitions, int kMeansIterations) {
+        LatLngDistanceFunction dist = new LatLngDistanceFunction(SphericalVincentyEarthModel.STATIC);
+        RandomlyGeneratedInitialMeans init = new RandomlyGeneratedInitialMeans(RandomFactory.DEFAULT);
+        KMeansLloyd<NumberVector> km = new KMeansLloyd<>(dist, kMeansPartitions, kMeansIterations, init);
+        km
+    }
+
+    private static StaticArrayDatabase getDatabase(double[][] data) {
+        DatabaseConnection dbc = new ArrayAdapterDatabaseConnection(data);
+        Database db = new StaticArrayDatabase(dbc, null);
+        db.initialize();
+        db
+    }
+
+    private static double[][] convertNodeList(ArrayList<Node> nodes) {
+        double[][] data = new double[nodes.size()][2];
+        for (int i = 0; i < data.length; i++) {
+            data[i][0] = nodes.get(i).getLat();
+            data[i][1] = nodes.get(i).getLon();
+        }
+        data
+    }
+}
