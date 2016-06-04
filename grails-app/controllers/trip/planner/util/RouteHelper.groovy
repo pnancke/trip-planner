9a833a510c402000c2a5950e457589529d874fb4
MZober1993 <Matthias.Zober1993@outlook.de>
2016-06-04 18:18:38 +0200

    Improve Types

---
+++ grails-app/controllers/trip/planner/util/RouteHelper.groovy
@@ -0,0 +1,21 @@
+package trip.planner.util
+
+import trip.planner.osm.api.Point
+
+import java.util.stream.Collectors
+
+
+class RouteHelper {
+
+    /**
+     * @see trip.planner.HomeController
+     * @param route - note: this type will be used in HomeController for the json request.
+     *
+     * @return points - List<Point>
+     */
+    public static List<Point> mapToPoints(List<List<String>> route) {
+        route.stream().map({ strings ->
+            new Point(Double.valueOf(strings.get(0)), Double.valueOf(strings.get(1)))
+        }).collect(Collectors.toList())
+    }
+}
